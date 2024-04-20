542057fe30c667c6034eec208eb2b42f2cff6f84
devjoaobatista0 <jvaraujobatista@gmail.com>
2024-04-20 02:27:20 -0300

    urls config

--- project/urls.py
+++ project/urls.py
@@ -26,3 +26,4 @@ urlpatterns = [
 ]
  #SERVINDO A MEDIA PARA CONSEGUIR VER AS IMAGENS DE UPLOAD ENQUANTO ESTA EM DESENVOLVIMENTO.
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
