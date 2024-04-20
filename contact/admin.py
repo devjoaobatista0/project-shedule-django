d0983066c01279e28ac339d5434c4a4be22d88a1
devjoaobatista0 <jvaraujobatista@gmail.com>
2024-04-20 14:12:40 -0300

    model ok

--- contact/admin.py
+++ contact/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from contact import models
 # Register your models here.
 
+
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone',
@@ -12,3 +14,10 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 200 #QUANTOS CONTATOS ATE APARECER O BOTAO DE 'MOSTRAR TUDO'
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone', 
+    
+    
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id', #ID DECRESCENTE
+ 
--- contact/models.py
+++ contact/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User #importando modulo de user para permitir que o 'owner'(proprietario) tenha permissoes
 # Create your models here.
 
 #TABELA
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category' #Toda vez que eu buscar o singular dessa categoria eu chamo de 'category'
+        verbose_name_plural = 'Categories' #Como deve ser chama rno plural
+    name = models.CharField(max_length=50)
 
+    
+    def __str__(self) -> str:
+        return self.name 
+    
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -13,6 +23,17 @@ class Contact(models.Model):
     description = models.TextField(blank = True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%y/%m/') #CRIANDO A FOLDER PICTURES COM O ANO E MES ATUAL
+    category = models.ForeignKey(  #criando model da categoria dependendo de contact
+        Category, #Model da categoria 
+        on_delete=models.SET_NULL, #quando eu deletar a categoria fazendo que o campo categoria daquele contato fique NULL
+        blank=True, null=True  
+    )
+    owner = models.ForeignKey(
+        User, 
+        on_delete = models.SET_NULL,
+        blank=True,
+        null = True,
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'  
