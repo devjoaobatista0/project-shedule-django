696404c7a0da6f8d3a49435ae9ee5a740a0776be
devjoaobatista0 <jvaraujobatista@gmail.com>
2024-04-20 02:26:57 -0300

    configs

--- project/urls.py
+++ project/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings #importando o settings para passar as variaveis de ambiente 
 
 urlpatterns = [
     path('', include('contact.urls')),
     path('admin/', admin.site.urls),
     
 ]
+ #SERVINDO A MEDIA PARA CONSEGUIR VER AS IMAGENS DE UPLOAD ENQUANTO ESTA EM DESENVOLVIMENTO.
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
