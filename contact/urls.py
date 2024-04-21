04a2f25c29de73b626bcbc686762a0d2064eb868
devjoaobatista0 <jvaraujobatista@gmail.com>
2024-04-21 02:20:56 -0300

    change in templates

--- contact/urls.py
+++ contact/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from contact import views
 
 
+app_name = 'contact'
+
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name = 'contact' ), #NOVA URL RECENDO O PARAMETRO PASSANDO O TIPO INT 'ID'
     path('', views.index, name = 'index' ),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from contact.models import Contact
+from django.http import Http404
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects\
+            .filter(show=True)\
+            .order_by('-id')[:10] #Ordenando por ordem descrecente e configurando o model 'show' como true para quando criar o contato, começar sempre
+                                                                 #mostrando, mas se eu quiser desmarcar o 'show' o contato nao aparecer na table.
+                                                                 
+    print(contacts.query) #VENDO TODAS AS QUERYS QUE O SERVIDOR FEZ.
+    
+    
     context = {
         'contacts': contacts,
     }
@@ -10,4 +18,27 @@ def index(request):
         request,
         'contact/index.html',
         context
+    )
+    
+    
+def contact(request, contact_id): #PASSANDO  O contact_id na url atraves do get
+    
+    single_contact = Contact.objects.filter(id=contact_id, show =True ).first() #RETORNA O UM UNICO VALOR
+    
+    #single_contact = get_list_or_404(       #POR ALGUM MOTIVO ESSA FUNC NAO ESTAVA RETORNANDO OS DADOS.
+        #Contact,pk=contact_id, show = True)
+    
+    
+    if single_contact is None:
+        raise Http404()
+    
+    context = {
+        'contact' : single_contact,
+    }
+    
+    return render(
+        request,
+        'contact/contact.html',
+        context,
+        
     )
