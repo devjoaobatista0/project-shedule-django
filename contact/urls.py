caabfe4d3df45b1a940ec6aceded13fe1c597820
devjoaobatista0 <jvaraujobatista@gmail.com>
2024-04-21 23:57:16 -0300

    configurando templates

--- contact/urls.py
+++ contact/urls.py
@@ -7,5 +7,6 @@ app_name = 'contact'
 
 urlpatterns = [
     path('<int:contact_id>/', views.contact, name = 'contact' ), #NOVA URL RECENDO O PARAMETRO PASSANDO O TIPO INT 'ID'
+    path('search', views.search, name = 'search' ), 
     path('', views.index, name = 'index' ),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, redirect
 from contact.models import Contact
 from django.http import Http404
+from django.db.models import Q
 # Create your views here.
 def index(request):
     contacts = Contact.objects\
@@ -13,12 +14,38 @@ def index(request):
     
     context = {
         'contacts': contacts,
+        'site_tittle': 'Contatos - '
     }
     return render(
         request,
         'contact/index.html',
         context
     )
+    
+def search(request):
+    search_value = request.GET.get('q', '').strip() #query dict 'q' = name do form.
+    if search_value == '':
+        return redirect('contact:index') #FAZENDO O REDIRECT PARA HOME CASO O USUARIO ENVIE O FORM VAZIO PARA EVITAR REQUEST INDESEJADA 
+    
+    contacts = Contact.objects\
+            .filter(show=True)\
+            .filter( Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value) | Q(phone__icontains=search_value) | Q(email__icontains=search_value) )\
+            .order_by('-id') #Ordenando por ordem descrecente e configurando o model 'show' como true para quando criar o contato, começar sempre
+                                  #mostrando, mas se eu quiser desmarcar o 'show' o contato nao aparecer na table.
+    #FILTER 'icontais' = ele passa um (case-sensitive) usado em buscas para nao diferenciar letra maiuscula de minuscla e tenta procurar o que o usuario digitar pelo contexto                                                             
+    print(contacts.query) #VENDO TODAS AS QUERYS QUE O SERVIDOR FEZ.
+    
+    
+    context = {
+        'contacts': contacts,
+        'site_tittle': 'Contatos - '
+    }
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )   
+    
     
     
 def contact(request, contact_id): #PASSANDO  O contact_id na url atraves do get
@@ -32,8 +59,11 @@ def contact(request, contact_id): #PASSANDO  O contact_id na url atraves do get
     if single_contact is None:
         raise Http404()
     
+    site_tittle = f'{single_contact.first_name} {single_contact.last_name} - ' #COLOCANDO O FIRST E O LAST NAME DO SINGLE CONTATO NO TITTLE DA URL
+    
     context = {
         'contact' : single_contact,
+        'site_tittle': site_tittle ,
     }
     
     return render(
